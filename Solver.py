dc318701475665ab4efeda0542e15ae2b3446a75
mirko <bicchierai.mirko.developer@gmail.com>
2024-02-28 19:11:11 +0100

    clear code

--- Solver.py
+++ Solver.py
@@ -62,7 +62,7 @@ class Solver:
 
     def sag_algorithm(self, f, dataset, epochs, learn_rate="L-LS"):
 
-        X, y = np.array(dataset.data_train, dtype="float128"), np.array(dataset.labels_train, dtype="float128")
+        x, y = np.array(dataset.data_train, dtype="float128"), np.array(dataset.labels_train, dtype="float128")
         n_samples, n_features = X.shape
 
         w = np.zeros(n_features, dtype="float128")
@@ -72,7 +72,7 @@ class Solver:
         x_plt, y_plt, x_times = [], [], []
         x_plt.append(0)
         x_times.append(0)
-        y_plt.append(f.loss_function(X, y, w))
+        y_plt.append(f.loss_function(x, y, w))
 
         v_index = list(range(n_samples))
 
@@ -80,33 +80,33 @@ class Solver:
             np.random.shuffle(v_index)
             start_time = time.time()
             for i in v_index:
-                g = f.loss_gradient(X[i:i + 1], y[i:i + 1], w)
+                g = f.loss_gradient(x[i:i + 1], y[i:i + 1], w)
                 d = d - memory[i] + g
                 memory[i] = g
                 if learn_rate == "L-LS":
-                    lr = (1 / 16 * self.lipschitz_estimate(f, X[i:i + 1], y[i:i + 1], w, n_samples))
+                    lr = (1 / 16 * self.lipschitz_estimate(f, x[i:i + 1], y[i:i + 1], w, n_samples))
                 else:
                     lr = learn_rate
                 w -= (lr / n_samples) * d
 
             x_plt.append(epoch)
-            y_plt.append(f.loss_function(X, y, w))
+            y_plt.append(f.loss_function(x, y, w))
             x_times.append((time.time() - start_time) + x_times[-1])
 
         return w, x_plt, y_plt, x_times, f.testing(dataset.data_test, dataset.labels_test, w)
 
-    def lipschitz_estimate(self, f, X, y, w, n):
+    def lipschitz_estimate(self, f, x, y, w, n):
 
         l_lip = 1
         max_iter = 50
         c = 0
-        old_loss = f.loss_function(X, y, w)
-        grad = f.loss_gradient(X, y, w)
+        old_loss = f.loss_function(x, y, w)
+        grad = f.loss_gradient(x, y, w)
         norm = pow(np.linalg.norm(grad), 2)
         if norm > pow(10, -8):
 
             new_w = w - (1 / l_lip) * grad
-            new_loss = f.loss_function(X, y, new_w)
+            new_loss = f.loss_function(x, y, new_w)
 
             while new_loss > old_loss - 1 / (2 * l_lip) * norm and c < max_iter:
                 l_lip = l_lip * 2  # (1 / pow(2, -(1 / n))) n, X.shape[0]
@@ -116,14 +116,14 @@ class Solver:
 
     def sag_algorithm_v2(self, f, dataset, epochs, lr="L-LS"):
 
-        X, y = np.array(dataset.data_train, dtype="float128"), np.array(dataset.labels_train, dtype="float128")
+        x, y = np.array(dataset.data_train, dtype="float128"), np.array(dataset.labels_train, dtype="float128")
         n_samples, n_features = X.shape
         w = np.zeros(n_features, dtype="float128")
 
         x_plt, y_plt, x_times = [], [], []
         x_plt.append(0)
         x_times.append(0)
-        y_plt.append(f.loss_function(X, y, w))
+        y_plt.append(f.loss_function(x, y, w))
 
         memory = np.zeros((n_samples, n_features))
         d = np.mean(memory, axis=0)
@@ -153,8 +153,8 @@ class Solver:
                 else:
                     learning_rate = lr
 
-                for j in range(len(X[idx])):
-                    if X[idx][j] == 0:
+                for j in range(len(x[idx])):
+                    if x[idx][j] == 0:
                         t[j] = t[j] + 1
                         agg[j] = -1
                     else:
@@ -164,7 +164,7 @@ class Solver:
                             agg[j] = t[j]
                             t[j] = 0
 
-                g = f.loss_gradient(X[idx:idx + 1], y[idx:idx + 1], w)
+                g = f.loss_gradient(x[idx:idx + 1], y[idx:idx + 1], w)
                 d = d - memory[idx] + g
                 memory[idx] = g
 
@@ -181,6 +181,6 @@ class Solver:
 
             x_times.append((time.time() - start_time) + x_times[-1])
             x_plt.append(epoch)
-            y_plt.append(f.loss_function(X, y, w))
+            y_plt.append(f.loss_function(x, y, w))
 
         return w, x_plt, y_plt, x_times, f.testing(dataset.data_test, dataset.labels_test, w)
